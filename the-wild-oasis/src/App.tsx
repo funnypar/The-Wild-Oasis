4d126f386c9cbfe379ab053d84ee428bbe86597a
Mopano <p.r.7.norouzi@gmail.com>
2026-06-25 15:17:27 +0300

    Add React Query to the application

--- the-wild-oasis/src/App.tsx
+++ the-wild-oasis/src/App.tsx
@@ -1,5 +1,6 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-
 import Account from './pages/Account';
 import Bookings from './pages/Bookings';
 import Cabins from './pages/Cabins';
@@ -11,9 +12,18 @@ import Users from './pages/Users';
 import { GlobalStyles } from './styles/GlobalStyles';
 import AppLayout from './ui/AppLayout';
 
+const queryClinet = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 60 * 1000,
+        },
+    },
+});
+
 function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClinet}>
+            <ReactQueryDevtools initialIsOpen={false} />
             <GlobalStyles />
             <BrowserRouter>
                 <Routes>
@@ -33,7 +43,7 @@ function App() {
                     <Route path='*' element={<PageNotFound />} />
                 </Routes>
             </BrowserRouter>
-        </>
+        </QueryClientProvider>
     );
 }
 
