64270d149c0777504d4814277d637038aa1bd6f2
Mopano <p.r.7.norouzi@gmail.com>
2026-06-28 20:48:59 +0300

    Implementation Of get setting

--- the-wild-oasis/src/features/settings/UpdateSettingsForm.tsx
+++ the-wild-oasis/src/features/settings/UpdateSettingsForm.tsx
@@ -1,24 +1,46 @@
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import Spinner from '../../ui/Spinner';
+import { useSettings } from './hooks/useSettings';
 
 function UpdateSettingsForm() {
-  return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
-      </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
-      </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
-      </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
-      </FormRow>
-    </Form>
-  );
+    const { isLoading, settings } = useSettings();
+
+    if (isLoading) return <Spinner />;
+
+    return (
+        <Form>
+            <FormRow label='Minimum nights/booking'>
+                <Input
+                    type='number'
+                    id='min-nights'
+                    defaultValue={settings?.minBookingLength}
+                />
+            </FormRow>
+            <FormRow label='Maximum nights/booking'>
+                <Input
+                    type='number'
+                    id='max-nights'
+                    defaultValue={settings?.maxBookingLength}
+                />
+            </FormRow>
+            <FormRow label='Maximum guests/booking'>
+                <Input
+                    type='number'
+                    id='max-guests'
+                    defaultValue={settings?.maxGuestsPerBooking}
+                />
+            </FormRow>
+            <FormRow label='Breakfast price'>
+                <Input
+                    type='number'
+                    id='breakfast-price'
+                    defaultValue={settings?.breakfastPrice}
+                />
+            </FormRow>
+        </Form>
+    );
 }
 
 export default UpdateSettingsForm;
